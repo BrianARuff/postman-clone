77ea6da115e2de5a6c5e9706f051ea37c3cc44a0
BrianARuff <briananthonyruff@gmail.com>
2021-06-23 21:35:12 -0400

    GET requst workings

--- src/Components/SearchButton/SearchButton.tsx
+++ src/Components/SearchButton/SearchButton.tsx
@@ -1,30 +1,72 @@
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { searchButton } from "../../content.json";
-import { setSearchItem } from "../../redux/actions/setSearchItem";
+import axios from "axios";
+import { useState } from "react";
+import ReactJsonPrint from "react-json-print";
 
 function SearchButton(props: any) {
-  const handleSetAddress = (e: any) => {
-    e.preventDefault();
-    const { setSearchItem, address } = props;
-    setSearchItem({ address: address });
+  const { queryType, searchAddress } = props;
+
+  const [response, setResponse] = useState({
+    responseData: {},
+  });
+
+  const mapAxiosRequest = (url = "", data = {}) => {
+    if (queryType === "GET")
+      return (
+        axios
+          .get(url)
+          // @ts-ignore
+          .then((res) => setResponse(res))
+          .catch((err) => setResponse(err))
+      );
+
+    if (queryType === "POST")
+      return axios
+        .post(url, data)
+        .then((res) => res)
+        .catch((err) => err);
+
+    if (queryType === "PUT")
+      return axios
+        .put(url, data)
+        .then((res) => res)
+        .catch((err) => err);
+
+    if (queryType === "PATCH")
+      return axios
+        .patch(url, data)
+        .then((res) => res)
+        .catch((err) => err);
+
+    if (queryType === "DELETE")
+      return axios
+        .delete(url, data)
+        .then((res) => res)
+        .catch((err) => err);
   };
-  return <button onClick={handleSetAddress}>{searchButton.text}</button>;
+
+  const handleHttpCall = () => {
+    return mapAxiosRequest(searchAddress, {});
+  };
+
+  return (
+    <>
+      <button onClick={handleHttpCall}>{searchButton.text}</button>
+      <br />
+      <br />
+      <ReactJsonPrint dataObject={response} />
+    </>
+  );
 }
 
 const mapStateToProps = (state: any) => {
-  return {
-    address: state.searchReducer.searchAddress,
-  };
+  return {};
 };
 
 const mapDispatchToProps = (dispatch: any) => {
-  return bindActionCreators(
-    {
-      setSearchItem,
-    },
-    dispatch
-  );
+  return bindActionCreators({}, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchButton);
