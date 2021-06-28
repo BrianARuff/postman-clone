27b58acd9fe2f6aaa94194f48f5e3e33f02a0cd4
BrianARuff <briananthonyruff@gmail.com>
2021-06-27 22:22:34 -0400

    changed to jsonviwer for my json tree

--- src/Components/SearchButton/SearchButton.tsx
+++ src/Components/SearchButton/SearchButton.tsx
@@ -2,9 +2,9 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { searchButton } from "../../content.json";
 import { useState } from "react";
-import ReactJson from "react-json-view";
 import { mapAxiosRequest } from "../../utilities/mapAxiosRequest";
 import { Button, Tooltip } from "@material-ui/core";
+import JSONViewer from "../JSONViewer/JSONViewer";
 
 function SearchButton(props: any) {
   const { queryType, searchAddress, jsonData } = props;
@@ -24,11 +24,7 @@ function SearchButton(props: any) {
         </Button>
       </Tooltip>
       <Tooltip title="Request Response">
-        <ReactJson
-          style={{ margin: "1rem" }}
-          src={response}
-          theme={"bright:inverted"}
-        />
+        <JSONViewer response={response} />
       </Tooltip>
     </>
   );
