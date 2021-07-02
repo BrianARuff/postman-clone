8bd858a2cf152d3426fcfce2e0de4622da9b9eec
BrianARuff <briananthonyruff@gmail.com>
2021-07-01 20:14:14 -0400

    verify post request works

--- src/utilities/mapAxiosRequest.ts
+++ src/utilities/mapAxiosRequest.ts
@@ -19,20 +19,20 @@ export const mapAxiosRequest = (
   if (queryType === "POST")
     return axios
       .post(url, data)
-      .then((res) => res)
-      .catch((err) => err);
+      .then((res) => setResponseData(res))
+      .catch((err) => setResponseData(err));
 
   if (queryType === "PUT") return axios.put(url, data).catch((err) => err);
 
   if (queryType === "PATCH")
     return axios
       .patch(url, data)
-      .then((res) => res)
-      .catch((err) => err);
+      .then((res) => setResponseData(res))
+      .catch((err) => setResponseData(err));
 
   if (queryType === "DELETE")
     return axios
       .delete(url, data)
-      .then((res) => res)
-      .catch((err) => err);
+      .then((res) => setResponseData(res))
+      .catch((err) => setResponseData(err));
 };
