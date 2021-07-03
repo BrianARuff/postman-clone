2736df61d30812b195113670f6394b069f005c9a
BrianARuff <briananthonyruff@gmail.com>
2021-07-03 05:24:26 -0400

    add storybook

--- src/Components/MainHeader/MainHeader.tsx
+++ src/Components/MainHeader/MainHeader.tsx
@@ -1,12 +1,13 @@
-import content from "../../content.json";
-import "./MainHeader.css";
-import { Typography } from "@material-ui/core";
+import content from '../../content.json';
+import './MainHeader.css';
+import { Typography } from '@material-ui/core';
 
 export default function MainHeader() {
-  const { mainHeader } = content;
-  return (
-    <Typography variant="h1" className={"main-header"}>
-      {mainHeader}
-    </Typography>
-  );
+   const { mainHeader } = content;
+   const { titleText } = mainHeader;
+   return (
+      <Typography variant="h1" className={'main-header'}>
+         {titleText}
+      </Typography>
+   );
 }
---
+++ src/stories/0-MainHeader.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import MainHeader from '../Components/MainHeader';
+
+export default {
+   title: 'Main Header',
+   component: MainHeader,
+};
+
+export const Title = () => <MainHeader />;
